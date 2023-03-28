15be3212138e65051699ef1abc6d4d8fec442bbc
piyerf5 <92050296+piyerf5@users.noreply.github.com>
2023-03-28 15:20:59 -0400

    k8s cluster resource added

--- vpc-site.tf
+++ vpc-site.tf
@@ -57,9 +57,14 @@ resource "volterra_aws_vpc_site" "example" {
     no_network_policy        = true
     no_forward_proxy         = true
     no_outside_static_routes = true
-    k8s_cluster              = true
+    // no_k8s_cluster           = true
     no_global_network        = true
     #default_storage         = ""
+     k8s_cluster {
+      namespace = "system"
+      name      = "pki-k8s"
+    }
+
     }
 
     // One of the arguments from this list "nodes_per_az total_nodes no_worker_nodes" must be set
@@ -73,6 +78,35 @@ resource "volterra_aws_vpc_site" "example" {
     }
 }
 
+resource "volterra_k8s_cluster" "example" {
+  name      = "pki-k8s"
+  namespace = "system"
+
+  // One of the arguments from this list "no_cluster_wide_apps cluster_wide_app_list" must be set
+  no_cluster_wide_apps = true
+
+  // One of the arguments from this list "use_custom_cluster_role_bindings use_default_cluster_role_bindings" must be set
+  use_default_cluster_role_bindings = true
+
+  // One of the arguments from this list "use_default_cluster_roles use_custom_cluster_role_list" must be set
+  use_default_cluster_roles = true
+
+  // One of the arguments from this list "cluster_scoped_access_deny cluster_scoped_access_permit" must be set
+  cluster_scoped_access_deny = true
+
+  // One of the arguments from this list "no_global_access global_access_enable" must be set
+  no_global_access = true
+
+  // One of the arguments from this list "no_insecure_registries insecure_registry_list" must be set
+
+  insecure_registry_list {
+    insecure_registries = ["example.com:5000"]
+  }
+  // One of the arguments from this list "no_local_access local_access_config" must be set
+  no_local_access = true
+  // One of the arguments from this list "use_default_psp use_custom_psp_list" must be set
+  use_default_psp = true
+}
 
 resource "volterra_cloud_site_labels" "labels" {
   name             = volterra_aws_vpc_site.example.name
